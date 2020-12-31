695259aff71fbdbd9874191f94e6825516d59abd
wike <cp_harris1011@hotmail.com>
2020-12-31 12:18:13 -0500

    begin adding data filler js

---
+++ dataPopulator.js
@@ -0,0 +1,8 @@
+let form = document.getElementsByTagName('form')[0];
+
+let nameField = form.getElementById('player1field');
+let timeHour = form.getElementById('hour');
+let timeMinute = form.getElementById('minute');
+let timeSecond = form.getElementById('second');
+let timeMillisecond = form.getElementById('milliseconds');
+let difficulty = form.getElementById('variable48006');
