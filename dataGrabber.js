02fb743ceeddf39f07daf1ecae42c88201a220b5
wike <cp_harris1011@hotmail.com>
2021-01-24 00:19:42 -0500

    we semi-automatic now

--- dataGrabber.js
+++ dataGrabber.js
@@ -5,17 +5,29 @@ let entries = document.getElementsByClassName("innerdata")[0];
 Array.from(entries.children[0].children).forEach((element, key) => {
     let data = {};
     let comment = element.getElementsByClassName("comment")[0];
-    if(comment) {
-        //console.log(element)
-        let heading = document.getElementById('heading').innerText;
-        data.difficulty = heading.includes('Very Hard') ? 'Very Hard' : heading.includes('Hard') ? 'Hard' : 'Normal';
-        data.name = heading.split("'")[0];
-        data.time = comment.innerText;
-        data.comment = comment.title;
-        element.addEventListener('click', () => {
-            chrome.storage.local.set({tscData: JSON.stringify(data)}, () => {
-                console.log("Entry set: "+ JSON.stringify(data))
-            });
-        })
+    if(!comment) {
+        comment = Array.from(element.children)[2].children[0];
     }
+    let dateElement = Array.from(element.children)[3];
+    let nameElement = Array.from(element.children)[1];
+    let heading = document.getElementById('heading').innerText;
+    data.difficulty = heading.includes('Very Hard') ? 'Very Hard' : heading.includes('Hard') ? 'Hard' : 'Normal';
+    data.name = nameElement.children[0].innerText;
+    data.time = comment.innerText;
+    data.comment = comment.title + " [Time Taken (with permission) from https://www.soniccenter.org/ ]";
+    data.date = dateElement.innerText;
+
+    // Create a button which copies all collected data to chrome local storage
+    newTableCol = document.createElement('td');
+    newTableCol.style = 'width: "200px";'
+    clickToCopy = document.createElement('button');
+    clickToCopy.addEventListener('click', (e) => {
+        chrome.storage.local.set({tscData: JSON.stringify(data)}, () => {
+            console.log("Entry set: "+ JSON.stringify(data))
+        });
+        e.target.disabled = true;
+    });
+    clickToCopy.innerText = 'Copy Data';
+    newTableCol.appendChild(clickToCopy);
+    element.appendChild(newTableCol);
 });
--- popup.js
+++ popup.js
@@ -3,19 +3,19 @@ let bigButton = document.getElementById('bigButton');
 let dataButton = document.getElementById('dataButton');
 
 let getData = (func) => {
-    chrome.storage.local.get('tscData', (data) => {
+    chrome.storage.local.get(['tscData'], (data) => {
         data = JSON.parse(data.tscData);
         //console.log(data)
         func(data);
     })
 };
-bigButton.addEventListener('click', () => {
-    getData((data) => {
-        console.log("this is the part where we auto-populate data :)")
-        //console.log(data);
-        console.log(document.children)
-    });
-})
+// bigButton.addEventListener('click', () => {
+//     getData((data) => {
+//         console.log("this is the part where we auto-populate data :)")
+//         //console.log(data);
+//         console.log(document.children)
+//     });
+// })
 
 dataButton.addEventListener('click', () => {
     getData((data) => {
